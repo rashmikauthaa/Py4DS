09750ef9de081feeed7c132cfcc93dfc834e0af3
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 11:41:36 +0000

    used data types

---
+++ dataT.py
@@ -0,0 +1,17 @@
+# Data types in python:
+
+# python don't have char data type
+
+# print([1,2,3,4,5]) #List->C->Array
+
+# print((1,2,3,4,5)) #Tuple
+
+# print({1,2,3,4,5}) #sets
+
+# print({
+#     'name ':'Autha',
+#     'college ':'SKIT',
+#     'age ':19
+# }) #Dictonary
+
+# print(type({1,2,3})) #Used to print datatype of given value
