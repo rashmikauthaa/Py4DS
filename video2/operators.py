8a1d89f0bf77fae1bd788ab854bc9ce2deef4d89
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 13:08:43 +0000

    learnt operator

---
+++ video2/operators.py
@@ -0,0 +1,8 @@
+# Arithmetic operators- (+,-,*,/,//)
+# Relational operators- (<,>,<=,==,!=)
+# Logical operator- (and,or,not)
+# Bitwise operator- (&,|,^,>>,<<)
+# ~ binary representation converts 1->0 nd visa versa
+# Assignment operator- (=)
+# Membership operator-  tells if in a given entity smthing is presnt or not (in, not in)
+print("D" in "Delhi")
