9774508ac4e6efb2c505a7b1281e4f3db678a539
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 11:54:21 +0000

    revisited variable

---
+++ variable.py
@@ -0,0 +1,17 @@
+# containers for future use
+
+# num1=3
+# num2=5
+# print(num1+num2)
+
+name="autha"
+# print(type(name))
+
+# we have Dynamic Typing in python, py interpreter is intelligent enough to understand the datatype given the value of variable
+# Static Typing- need to declare datatype before using, C/C++,JAVA
+
+print(name)
+name=19
+print(name)
+# Dynamic Binding, in a prog one variable can be used for diff. datatypes
+# Static Binding, we have in C/C++, one variable one datatype FIXED
