343098dfbd7727ea2d452ed1cc7bab57925ff954
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 12:04:22 +0000

    HandsOn keyword and Identifiers

---
+++ keyWpID.py
@@ -0,0 +1,5 @@
+# Keywords- Reserved words, can't use these words as variables
+# helps interpreter to convert high level lang->Machine lang
+# 32 keywords
+
+# Identifiers- The variables we create 
