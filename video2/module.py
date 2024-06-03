5c4f8da19948bead884b4b5f974838baa804fb3e
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 13:28:48 +0000

    exploring modules

---
+++ video2/module.py
@@ -0,0 +1,16 @@
+# modules are used to utlize the builtin code, and do some operations
+
+import math
+# print(math.factorial(5))
+
+import keyword
+# print(keyword.kwlist)
+
+import random
+# print(random.randint(1,100))
+
+import datetime
+# print(datetime.datetime.now())
+
+help('modules')
+# helps to print all the modules
