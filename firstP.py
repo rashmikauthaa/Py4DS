bb471a2d66ed63dfc6adadf8d2d4da01803c9b97
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 12:15:30 +0000

    first program

---
+++ firstP.py
@@ -0,0 +1,10 @@
+fnum = input("enter num1: ")
+snum = input("enter num2: ")
+
+# ans=fnum+snum
+# print(ans) 
+# They are not added mathamatically, added laterally :(
+
+print(type(fnum)) # considers as strings
+
+# strings are universal format in python
