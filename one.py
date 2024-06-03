b57148946a4a294b727eb2f0b1544ea7cd3ae92a
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-03 11:31:07 +0000

    startedWdPython

---
+++ one.py
@@ -0,0 +1,10 @@
+# python is case sensitive
+print("Hello World")
+print(7)
+
+print(1e309) # max value of int
+
+print("hello",123,"true",sep=':',end="-") # we can change the documentation content: FLEXIBLE
+print("hey")
+# print(true)
+# print(True)
