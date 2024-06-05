9c74514b2a778c77558b126e5d67b9eb76e4f322
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-05 13:50:52 +0000

    functions on strings

---
+++ video3/functions.py
@@ -0,0 +1,16 @@
+# string functions
+
+# print(len('hello world'))
+# print(max('hello world'))
+# print(min('hello world')) # space
+# print(sorted('hello world'))
+# print(sorted('hello world',reverse=True))
+
+s='heLlo wOrlD'
+# print(s.capitalize()) # first char of string is capitalized
+# print(s.title()) # every word is capitalized
+# print(s.upper()) # all char to upper case
+# print(s.lower()) # all char to lower case
+# print(s.swapcase()) # caps to low and visa versa
+
+'hello world'.find('j')
