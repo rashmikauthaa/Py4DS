52722e8352edb818c86a82897e543c8e2df4194a
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-05 13:39:42 +0000

    string operators

---
+++ video3/operators.py
@@ -0,0 +1,24 @@
+# 1. Arithmetic Operators
+# print('delhi'+'mumbai')
+# print('delhi'+' '+'mumbai')
+# print('goa'*3)
+
+# 2. Relational Operators
+# print('delhi'=='delhi')
+# print('mp'=='MP')
+
+# They are comapered lexiographically, upon their ascii values
+# print('mumbai'>'pune')
+# print('pune'>'mumbai')
+# print('Pune'>'pune')
+
+# 3. Logical Operators
+# if string has chars it is true, empty false
+# print('hello'and'world')
+# print('hello'or'world')
+
+for i in 'delhi':
+    print('pune')
+
+# 4. Membership Operators   
+print('D' in 'delhi' )
