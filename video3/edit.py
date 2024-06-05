86335c7eeaa4915e346fc87762b0f30b9ca0d306
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-05 12:52:47 +0000

    edit and delete

---
+++ video3/edit.py
@@ -0,0 +1,10 @@
+s='hello world'
+# s[0]='H'
+print(s)
+# TypeError: 'str' object does not support item assignment
+# python strings are immutable: can't change once after assigning
+
+
+# del operator used to delete a string??
+del s 
+print(s)
