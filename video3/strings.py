a89aabb68499f37edef940f7d4dd607f5f9022ae
rashmikauthaa <164472969+rashmikauthaa@users.noreply.github.com>
2024-06-05 12:47:18 +0000

    bit about strings

---
+++ video3/strings.py
@@ -0,0 +1,21 @@
+# strings are collection of characters.
+s='autha'
+s=str('shambu')
+# print(s)
+
+# we have indexing to access characters of a string
+# we have (+)ve indexing --->
+# print(s[0])
+# we have (-)ve indexing <---
+# print(s[-1]) # we can find last char of a str
+
+s='hello world'
+# SLICING is used to extract more than one char
+# print(s[0:5])
+# print(s[2:3])
+# print(s[2:])
+# print(s[:3])
+# print(s[0:6:2]) # step size, for skipping
+
+# print(s[::-1]) # one liner to reverse a str
+print(s[-5:])
